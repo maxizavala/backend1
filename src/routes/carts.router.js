2386054d5a8c754a793ec3aa1873e68981c6d21a
Maxi Bleu <maximiliano.zavala@davinci.edu.ar>
2024-07-24 15:14:40 -0300

    carts.router: POST crear carrito

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,13 +1,40 @@
 import { Router } from "express";
+import fs from "fs";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = Router();
+const cartPath = "./src/data/carts.json";
 
-router.get("/api/carts", (req, res) => {
-    res.send("carts");
-})
-
+// RUTAS
+// Crea un carrito
 router.post("/api/carts", (req, res) => {
-    res.send({status: "success", message: "carts"});
+    try {
+        let { products = [] } = req.body;
+
+        // Nuevo carrito
+        const newCart = {
+            id: uuidv4(),
+            products: products
+        };
+
+        saveCart(newCart);
+        res.send({status: "success", message: "Carrito creado exitosamente"});
+    } catch (error) {
+        res.status(500).send({status: "error", message: "Error al guardar el carrito"});
+    }
 })
+
+
+// FUNCIONES
+const saveCart = async (newCart) => {
+    try {
+        const data = await fs.promises.readFile(cartPath, 'utf-8');
+        const carts = JSON.parse(data);
+        carts.push(newCart);
+        await fs.promises.writeFile(cartPath, JSON.stringify(carts, null, 2), 'utf-8');
+    } catch (error) {
+        console.error('Error al guardar el carrito:', error);
+    }
+};
 
 export default router;
