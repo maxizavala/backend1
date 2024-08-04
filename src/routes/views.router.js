1aed1ce09b650b62d59811d5da15590e94e21818
Maxi Bleu <maximiliano.zavala@davinci.edu.ar>
2024-08-04 19:45:34 -0300

    home.handlebars

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import fs from "fs";
 
 const router = Router();
+const productsPath = "./src/data/products.json";
 
-// Rutas
-router.get("/views/home", (req, res) => {
-    res.render("home");
+// RUTAS
+// Muestra todos los productos de la base
+router.get("/views/home", async (req, res) => {
+    try {
+        const data = await fs.promises.readFile(productsPath, 'utf-8');
+        const products = JSON.parse(data);
+        res.render("home", { products });
+    } catch (error) {
+        res.status(500).send({status: "error", message: "Error al leer el archivo"});
+    }
 })
 
 export default router;
