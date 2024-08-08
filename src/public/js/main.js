1d1061cce73352d910ea5415279d349260e841bc
Maxi Bleu <maximiliano.zavala@davinci.edu.ar>
2024-08-08 12:34:40 -0300

    add bootstrap styles

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,12 +7,16 @@ socket.on("products", (data) => {
     listaProductos.innerHTML = '';
     data.forEach(producto => {
         listaProductos.innerHTML += `
-            <li>
-                <h2>${producto.title}</h2>
-                <p>${producto.description}</p>
-                <p>${producto.category}</p>
-                <p>Precio: $${producto.price}</p>
-            </li>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.title}</h5>
+                        <p class="card-text">${producto.description}</p>
+                        <p class="card-text"><small class="text-muted">${producto.category}</small></p>
+                        <p class="card-text"><strong>Precio: $${producto.price}</strong></p>
+                    </div>
+                </div>
+            </div>
         `;
     });
 });
